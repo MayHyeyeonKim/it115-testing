576eb07511ba0c6065f4161bb44bc3d3dd15f052
MayHyeyeonKim <hkim7963@gmail.com>
2022-12-08 21:51:02 -0800

    final class

--- math.js
+++ math.js
@@ -14,11 +14,31 @@ function div(a,b){
     return a / b ;
 };
 
+function between(a,b) {
+    if(a > b) {
+        return a + " is greater than " + b;
+    } else if (b > a) {
+        return b + " is greater than " + a;
+    } else {
+        return a + " is equal to " + b;
+    }
+}
 
+function colors(color){
+    if(color == "purple") {
+        return "I love " + color;
+    } else if (color == "red") {
+        return color + " is okay";
+    } else {
+        return "I hate " + color;
+    }
+}
 
 module.exports = {
     add,
     sub,
     mult,
-    div
+    div,
+    between,
+    colors
 };
--- math.test.js
+++ math.test.js
@@ -20,3 +20,30 @@ test('divides 17 / 3 to equal 5.666... ', () => {
     expect(utils.div(17,3)).toBeCloseTo(5.667);
 
 });
+
+test('should return:"6 is greater then 4"', () => {
+    expect(utils.between(6,4)).toBe("6 is greater than 4");
+
+});
+
+test('should return:"6 is greater then 4"', () => {
+    expect(utils.between(4,6)).toBe("6 is greater than 4");
+
+});
+
+test('should return:"4 is equal to 4"', () => {
+    expect(utils.between(4,4)).toBe("4 is equal to 4");
+
+});
+
+test('should return: "I love purple"', () => {
+    expect(utils.colors("purple")).toBe("I love purple");
+});
+
+test('should return: "red is okay"', () => {
+    expect(utils.colors("red")).toBe("red is okay");
+});
+
+test('should return: "I hate green"', () => {
+    expect(utils.colors("green")).toBe("I hate green");
+});
